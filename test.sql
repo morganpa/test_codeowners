65c6a8a45c05072a0a438b294331a689a3dd988f
morganpa <66316054+morganpa@users.noreply.github.com>
2021-06-14 12:57:28 -0700

    test

--- test.sql
+++ test.sql
@@ -1,1 +1,1 @@
-SELECT * FROM breaking_change where "id" = "EVIL DIRTY CHANGE"
+SELECT * FROM breaking_change where "id" = "evil dirty change that is totally going to break everything if john doesn't stop me"
